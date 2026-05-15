5defeddabf5de1adc38bac85788b5e2b36dfa829
Raul <raul@whoisrgj.com>
2026-05-15 13:17:25 -0500

    disable docs endpoint in production

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 
+from src.config import settings
 from src.posts.router import router as posts_router
 
-app = FastAPI()
+SHOW_DOCS_IN = {"dev"}
+app_kwargs: dict = {"title": "whoisrgj.com API"}
+if settings.ENVIRONMENT not in SHOW_DOCS_IN:
+    app_kwargs["openapi_url"] = None
+
+app = FastAPI(**app_kwargs)
 
 app.include_router(posts_router)
 
 
 @app.get("/")
 async def index():
-    return "whoisrgj.com api v1"
+    return "whoisrgj.com API"
