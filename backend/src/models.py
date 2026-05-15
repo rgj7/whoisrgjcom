1dc22703c422dee2b5ce84e1934f42024387d0d3
Raul <raul@whoisrgj.com>
2026-05-15 13:17:52 -0500

    use postgres naming convention

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,5 +1,14 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+
+POSTGRES_NAMING_CONVENTION = {
+    "ix": "%(column_0_label)s_idx",
+    "uq": "%(table_name)s_%(column_0_name)s_key",
+    "ck": "%(table_name)s_%(constraint_name)s_check",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_pkey",
+}
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=POSTGRES_NAMING_CONVENTION)
